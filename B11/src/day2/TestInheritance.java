88551d7333f56ed5352afa6001af92b5cc843189
Naveen <Naveen@DESKTOP-IR9KMPD>
2022-03-09 12:23:27 +0530

    push inheritance

---
+++ B11/src/day2/TestInheritance.java
@@ -0,0 +1,19 @@
+package day2;
+
+class Animal{  
+void eat(){System.out.println("eating...");}  
+}  
+class Dog extends Animal{  
+void bark(){System.out.println("barking...");}  
+}  
+class Cat extends Animal{  
+void meow(){System.out.println("meowing...");}  
+}  
+public class TestInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
